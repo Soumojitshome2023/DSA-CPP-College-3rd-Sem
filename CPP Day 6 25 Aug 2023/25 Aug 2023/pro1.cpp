b2309378bc19686e600f5e3281e2c037ff61039a
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-08-27 11:08:34 +0530

    readme add

--- CPP Day 6 25 Aug 2023/25 Aug 2023/pro1.cpp
+++ CPP Day 6 25 Aug 2023/25 Aug 2023/pro1.cpp
@@ -1,87 +1,78 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
+// Sparac Matrix
 
 using namespace std;
 
-int main(){
-	int m, n, i, j, no_of_zero=0;
-	cout<<"\nEnter number of row m =  ";
-	cin>>m;
-	cout<<"\nEnter number of column n =  ";
-	cin>>n;
+int main()
+{
+	int m, n, i, j, no_of_zero = 0;
+	cout << "\nEnter number of row m =  ";
+	cin >> m;
+	cout << "\nEnter number of column n =  ";
+	cin >> n;
 	int matrix[m][n];
-	for(i=0; i<m; i++){
-		for(j=0; j<n; j++){
-			cout<<"\nEnter the element for row : "<<i+1<<" column : "<<j+1<<" : ";
-			cin>>matrix[i][j];
-			if(matrix[i][j] == 0)
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			cout << "\nEnter the element for row : " << i + 1 << " column : " << j + 1 << " : ";
+			cin >> matrix[i][j];
+			if (matrix[i][j] == 0)
 				no_of_zero++;
-
-		}			
+		}
 	}
 
-	cout<<"Your Matrix : "<<endl;
-	for(i=0; i<m; i++){
-		for(j=0; j<n; j++){
-			cout<<matrix[i][j]<<"\t";
-
+	cout << "Your Matrix : " << endl;
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			cout << matrix[i][j] << "\t";
 		}
-		cout<<endl;			
+		cout << endl;
 	}
 
-	if( no_of_zero > ((m*n)/2) ){
-		cout<<"Sparac Matrix";
+	if (no_of_zero > ((m * n) / 2))
+	{
+		cout << "Sparac Matrix";
 
-		cout<<"\nRow     |     Column     |     Value";
-		cout<<"\n--------------------------------------";
-		for(i=0; i<m; i++){
-			for(j=0; j<n; j++){
-				if(matrix[i][j] != 0){
-					cout<<"\n"<<"   "<<i+1<<"\t\t"<<j+1<<"\t\t"<<matrix[i][j];
-				}	
-			}		
+		cout << "\nRow     |     Column     |     Value";
+		cout << "\n--------------------------------------";
+		for (i = 0; i < m; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (matrix[i][j] != 0)
+				{
+					cout << "\n"
+						 << "   " << i + 1 << "\t\t" << j + 1 << "\t\t" << matrix[i][j];
+				}
+			}
 		}
-		cout<<endl;
-		/*cout<<"\nColumn : ";
+		cout << endl;
+		/*
+		cout<<"\nColumn : ";
 		for(i=0; i<m; i++){
 			for(j=0; j<n; j++){
 				if(matrix[i][j] != 0){
 					cout<<j+1<<" ";
-				}	
-			}		
+				}
+			}
 		}
 		cout<<"\nValue  : ";
 		for(i=0; i<m; i++){
 			for(j=0; j<n; j++){
 				if(matrix[i][j] != 0){
 					cout<<matrix[i][j]<<" ";
-				}	
-			}		
-		}	
+				}
+			}
+		}
 
 	*/
-
 	}
 	else
-		cout<<"Not a Sparac Matrix";
+		cout << "Not a Sparac Matrix";
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ readme.md
@@ -0,0 +1,11 @@
+### 1. Day 1 : Basic CPP
+
+### 2. Day 2 : Stack | Push, Pop, Display
+
+### 3. Day 3 : 
+
+### 4. Day 4 : Queue
+
+### 5. Day 5 : Circular Queue
+
+### 6. Day 6 : Sparac Matrix
