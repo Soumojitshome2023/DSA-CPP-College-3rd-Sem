7114fddc67ec7e45c5dc1a0c17b7e8204f7af0fa
rick-03 <satwick.edu@gmail.com>
2023-09-09 08:26:13 +0530

    prefix to infix add using stack

---
+++ CPP Day 7 1 Sep 2023/prefix_to_infix.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool isOperand(char c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c>='0' && c<='9')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+string PrefixToInfix(string prefix)
+{
+    stack<string> s;
+    for (int i = prefix.length() - 1; i >= 0; i--) {
+        if (isOperand(prefix[i])) {
+            string op(1, prefix[i]);
+            //string op = prefix[i];
+            s.push(op);
+        }
+        else {
+            string op1 = s.top();
+            s.pop();
+            string op2 = s.top();
+            s.pop();
+            s.push("(" + op1 + prefix[i] + op2 + ")");
+        }
+    }
+    return s.top();
+}
+
+int main()
+{
+    string infix, prefix;
+    cout << "Enter a PREFIX Expression :" << endl;
+    cin >> prefix;
+    cout << "PREFIX EXPRESSION: " << prefix << endl;
+    infix = PrefixToInfix(prefix);
+    cout << endl
+         << "INFIX EXPRESSION: " << infix;
+
+    return 0;
+}
